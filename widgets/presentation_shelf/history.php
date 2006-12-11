1212e50c8f4b6be02ea391f9a72e54759f4cce90
cgross <cgross>
2006-12-11 20:07:49 +0000

    *** empty log message ***

---
+++ widgets/presentation_shelf/history.php
@@ -0,0 +1,51 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# history.php
+	#
+	# Author: 		Chris Gross
+	# Date:			2006-05-07
+	#
+	# Description: Describes the history of Grid.
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Nebula PShelf-based Presentation Update History";
+	$pageKeywords	= "Nebula, SWT, Widgets, Controls, Snippets";
+	$pageAuthor		= "Chris Gross";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+
+		<h3>12/11/06 Update</h3>
+		<ul>
+		<li>Initial build.</li>
+		</ul>
+
+		<hr class="clearer"/>
+	</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>

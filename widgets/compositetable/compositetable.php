f18543ab254b0240a3fbb9165a277066a07f0add
dorme <dorme>
2006-12-13 14:08:53 +0000

    Initial content

--- widgets/compositetable/compositetable.php
+++ widgets/compositetable/compositetable.php
@@ -14,9 +14,9 @@
 	
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Nebula CTableTree Widget";
-	$pageKeywords	= "Nebula, SWT, Widgets, Controls";
-	$pageAuthor		= "Jeremy Dowdall";
+	$pageTitle 		= "Nebula CompositeTable Widget";
+	$pageKeywords	= "Nebula, SWT, Widgets, Controls, CompositeTable";
+	$pageAuthor		= "David Orme";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
@@ -33,32 +33,37 @@
 
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
-		<h2>CTableTree</h2>
+		<h2>CompositeTable</h2>
 		
-		<img src='ctabletree.png'>
+		<img src='images/ctable-sn3.PNG'><br/>
+		<img src='images/ctable-addrblock.PNG'>
 		<p>
-		The CTableTree widget is a custom TableTree component created with two primary
-		purposes.
+		CompositeTable is a custom SWT grid control that:
 		<ol>
-			<li>Allow items to expand and collapse independently of one another</li>
-			<li>Separate the creation and maintenance of Cells from the implementation of the TableTree</li>
+			<li>Gives you control over the layout of your rows</li>
+			<li>Automatically edits in place using any SWT control</li>
+			<li>Manages CRUD operations simply and automatically</li>
 		</ol>
 		</p>
 		<p>
-		At its heart, the CTableTree widget is simply a container for custom cells which are
-		organized by items and displayed in a manner similar to a SWT TableTree.  More
-		importantly though, the custom cells may become quite complex and have a great deal of
-		functionality beyond just displaying data.  For example, a cell can be written to hold
-		a fully functional IM session.  By placing these cells into a CTableTree a user could
-		scroll through many sessions, expanding and collapsing them as convenient.  Further more,
-		the cell and the implementation of the containing CTableTree are not tightly coupled so
-		they do not have to be in the same Eclipse Plug-in - or maintained by the same programmer.
+		CompositeTable is designed to solve 80% of the problems faced by RCP
+		developers creating CRUD interfaces that aren't solved by Eclipse 
+		Data Binding.</p>
+		<p>
+		It does this though two strategies:  (a) CompositeTable enables you 
+		to design your grid's row control as a custom SWT control.  Your row
+		control can be anything and can contain any other SWT control.
+		SWT controls are in an always-on, always-editing state all the time.
+		This simplifies the user's experience with the application considerably
+		while giving you great control over your application's layout.
+		(b) Providing an event model with features like two-phase commits
+		making it simple to map CompositeTable events to CRUD database semantics.
 		</p>
 
 		<p>
-		NOTE: The CTableTree widget is still under active development.<br>
-		The code is made available in an
-		<font color=red><b>ALPHA</b></font> state.  Users should expect changes in future versions.
+		NOTE: The CompositeTable widget is still under active development.<br>
+		The code is made available in a
+		<font color=red><b>BETA</b></font> state.  Users should expect changes in future versions.
 		</p>
 		
 		<p>

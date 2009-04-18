c1be4f9e8796ce66a8cbf4360cd9e5b9a12c6279
rnicolas <rnicolas>
2009-04-18 16:36:53 +0000

    Added Note

--- downloads.php
+++ downloads.php
@@ -36,8 +36,7 @@
 	
 	
 	<div style="background-color: #fdf; border: red dotted 2px; padding:10px">
-			<p>
-			    Note: When using update site on some Eclipse versions (3.5M6), please uncheck 'group by categories' to make the feature appear in the update manager.</p></div>
+	    Note: When using update site on some Eclipse versions (3.5M6), please uncheck 'group by categories' to make the feature appear in the update manager.</div>
 	
 	<h2>Nebula Examples</h2>
 		<ul>

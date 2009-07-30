ba468edd85b73cee0a4d6faf717d5c49fe04eda7
rnicolas <rnicolas>
2009-07-30 09:41:01 +0000

    Added downloads from radiogroup

--- downloads.php
+++ downloads.php
@@ -120,12 +120,8 @@
 	    <a name="RadioGroup"></a>
 	    <h2>RadioGroup</h2>
 		<ul>
-			<li>Latest nightly build : <font color='red'>Not yet available</font></li>
-			<li>Update site (nightly builds) : <font color='red'>Not yet available</font></li>
-			<!--
-			<li>Latest nightly build : <a href="http://www.eclipse.org/downloads/download.php?file=/technology/nebula/pshelf/downloads/drops/latest/nebula-radiogroup-SDK-incubation.zip">nebula-pshelf-SDK-incubation.zip</a></li>
+			<li>Latest nightly build : <a href="http://www.eclipse.org/downloads/download.php?file=/technology/nebula/radiogroup/downloads/drops/latest/nebula-radiogroup-ALL-incubation.zip">nebula-radiogroup-ALL-incubation.zip</a></li>
 			<li>Update site (nightly builds) : http://download.eclipse.org/technology/nebula/radiogroup/update-N/</li>
-			-->
 	    </ul>
 	    
 	    <a name="CWT"></a>

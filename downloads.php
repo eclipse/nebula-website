68defa6c66bbc866fd9c1a6793f893999fdae014
rnicolas <rnicolas>
2009-08-04 12:48:19 +0000

    Updated gallery downloads

--- downloads.php
+++ downloads.php
@@ -90,12 +90,12 @@
 	    <a name="Gallery"></a>
 		<h2>Gallery</h2>
 		<ul>
-			<li>Archive</li>
+			<li><b>Files</b></li>
 				<ul>
 					<li>0.5.2 (stable) : <a href="http://www.eclipse.org/downloads/download.php?file=/technology/nebula/gallery/downloads/drops/R_0_20090418/nebula-gallery-ALL-incubation-0.5.2_R_0_20090418.zip">nebula-gallery-ALL-incubation-0.5.2_R_0_20090418.zip</a> - <a href="http://www.eclipse.org/nebula/widgets/gallery/releasenotes/r_0_20090418/releasenotes.php">Notes</a></li>
 				<li> Nightly builds : <a href="http://download.eclipse.org/technology/nebula/gallery/downloads/drops/">Build list</a> (test results included)</li>
 				</ul>
-			<li>Update site</li>
+			<li><b>Update sites</b></li>
 				<ul>
 					<li>Stable : http://download.eclipse.org/technology/nebula/gallery/update/   </li>
 					<li>Nighlty : http://download.eclipse.org/technology/nebula/gallery/update-N/  </li>

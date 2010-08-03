422a6aa9f708c2efa75032d7550fdedb0dd8409a
mjones <mjones>
2010-08-03 11:54:42 +0000

    updated text to show the true repository location for the Nebula source.

--- source.php
+++ source.php
@@ -35,7 +35,8 @@
 		<h1>$pageTitle</h1>
 		<div style="margin:5px 0 15px 15px">
 			<p>This project hosts its CVS repository at:</p>
-			<p style="margin-left:15px">:pserver:anonymous@dev.eclipse.org:/cvsroot/technology/org.eclipse.swt.nebula</p>
+			<p style="margin-left:15px">:pserver:anonymous@dev.eclipse.org:/cvsroot/technology</p>
+			<p style="margin-left:30px">The source is located under the package org.eclipse.swt.nebula</p>
 			<p style="padding-top:15px">For information on using CVS at Eclipse.org, please visit the <a href="http://wiki.eclipse.org/index.php/CVS_Howto">wiki page</a></p>
 			<p>The CVS repository is also browseable <a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.swt.nebula/?root=Technology_Project">on the web</a>.</p>
 			<p>For a quick start, use one of the <a href="http://wiki.eclipse.org/Nebula_Setup">Team Project Sets</a> to download all Nebula projects from CVS.</p>

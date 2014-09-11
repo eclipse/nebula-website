e8bb67ae3aa316024f5f6ea2229b4f470374ac9b
Wim Jongman <wim.jongman@remainsoftware.com>
2014-09-11 10:49:16 -0400

    Bug 443429 - Make a stable set of downloads.

--- downloads.php
+++ downloads.php
@@ -1,17 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-
-	#*****************************************************************************
-	#
-	# examples.php
-	#
-	# Author: 		Jeremy Dowdall
-	# Date:			2009-02-01
-	#
+	# Author: 		Jeremy Dowdall / Wim Jongman / Mickael Istria
 	# Description:  Nebula Download page.
-	#
-	#
-	#****************************************************************************
-	
+
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Nebula Downloads";
@@ -33,74 +23,77 @@
 
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
-	
-	
-	<h2>Nebula Release Latest <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229894' title='Drag and drop into a running Eclipse Indigo(+) workspace to install Eclipse Nebula Stable Widgets'> 
-	<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></h2>
-</a>
+		<br/>
+		
+		<h2>Latest</h2>
 
-	<div
-		style="background-color: orange; border: red dotted 2px; padding: 10px">
-		<p style="font-weight: bold">We are working towards a set of
-			widgets that are production ready, stable and maintained.</p>
-		<p>
-			This set of widgets is known as "Nebula Release". The widgets that
-			are proposed to be in "Nebula Release" are listed on the left. This
-			update site contains the latest version of the widgets.Our CI build
-			flows from check-in to publish on the Eclipse Download site. Use this
-			URL to <a
-				href="http://help.eclipse.org/indigo/index.jsp?topic=%2Forg.eclipse.platform.doc.user%2Ftasks%2Ftasks-129.htm">learn how to install</a>
-			"Nebula Release" widgets in your Eclipse Platform:
-		</p>
-		<div
-			style="background-color: #fdf; border: red dotted 2px; padding: 10px">
-			<center>
-				<p style="font-weight: bold">
+		<h4>Nebula Release (12 Projects) <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229894' title='Drag and drop into a running Eclipse to install these Widgets'> 
+		<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></a></h3>
+			<p style="font-weight: bold">
 				<ul>
-				<li>Update site: <a href="http://download.eclipse.org/technology/nebula/snapshot">http://download.eclipse.org/technology/nebula/snapshot</a></li> 
-				<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/snapshot/repository.zip">repository.zip</a></li>
-				<li><a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229894' title='Drag and drop into a running Eclipse Indigo(+) workspace to install Eclipse Nebula Stable Widgets'>Drag this link to Eclipse installation to install</a></li>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/snapshot">http://download.eclipse.org/technology/nebula/snapshot</a></li>
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/snapshot/repository.zip">repository.zip</a></li>
+					<li><a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229894' title='Drag and drop into a running Eclipse to install these Widgets'>Drop this link into Eclipse to install</a></li>
 				</ul>
-				</p>
-			</center>
-			
-		</div>
-	</div>
+			</p>
 
-	<h2>Nebula Incubation Latest <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229909' title='Drag and drop into a running Eclipse Indigo(+) workspace to install Eclipse Nebula Incubation Widgets'> 
-	<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></h2>
-</a>
-
-	<div
-		style="background-color: orange; border: red dotted 2px; padding: 10px">
-		<p style="font-weight: bold">The widgets are in incubation because
-			they are in an early state of development or because we do not have
-			an active committers for the widget.</p>
-		<p>
-			Some widgets are stable enough for production use. Please see the
-			individual widget pages for more status information. You will find
-			latest version of widgets, with their new feature and more recent bug
-			fixes. However, since they are development build, they may include
-			some bugs that are not present in releases. Test these widgets with
-			your use case before you release them in production. Use this URL to
-			<a
-				href="http://help.eclipse.org/indigo/index.jsp?topic=%2Forg.eclipse.platform.doc.user%2Ftasks%2Ftasks-129.htm">leran how to install</a>
-			"Nebula Incubation" widgets in your Eclipse Platform:
-		</p>
-		<div
-			style="background-color: #fdf; border: red dotted 2px; padding: 10px">
-			<center>
-				<p style="font-weight: bold">
+		<h4>Nebula Incubation (9 Projects) <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229909' title='Drag and drop into a running to install these Widgets'> 
+		<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></a></h3>
+			<p style="font-weight: bold">
 				<ul>
-				<li>Update site: <a href="http://download.eclipse.org/technology/nebula/incubation/snapshot">http://download.eclipse.org/technology/nebula/incubation/snapshot</a></li>
-				<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/incubation/snapshot/repository.zip">repository.zip</a></li>
-				<li><a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229909' title='Drag and drop into a running Eclipse Indigo(+) workspace to install Eclipse Nebula Incubation Widgets'>Drag this link to Eclipse installation to install</a></li>
-				</p>
-			</center>
-		</div>
-	</div>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/incubation/snapshot">http://download.eclipse.org/technology/nebula/incubation/snapshot</a></li>
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/incubation/snapshot/repository.zip">repository.zip</a></li>
+					<li><a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229909' title='Drag and drop into a running Eclipse to install these Widgets'>Drop this link into Eclipse to install</a></li>
+				</ul>
+			</p>
+			
+<!-- ------------------ Older downloads  -->
+			<br/>
+			<br/>
+			<h2>Archives</h2>
+			This section contains archived repositories so that release engineers do not have to
+			change their builds so often. Please note that you don't have to depend on a 
+			specific build qualifier in your target definition or feature. If you still want to do that
+			and don't want to update your build scripts every day then make a local copy of 
+			a repo or choose on of these below.
+			<br/>
+			<h3>Q3/2014. Retention period: 2 years.</h3> 
+			<h4>Nebula Release (12 Projects)</h4>
+			<p style="font-weight: bold">
+				<ul>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/archives/Q32014/release/">http://download.eclipse.org/technology/nebula/archives/Q32014/release/</a></li> 
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/archives/Q32014/release/repository.zip">repository.zip</a></li>
+				</ul>
+			</p>
 
-	<hr class="clearer" />
+			<h4>Nebula Incubation (9 Projects)</h4>
+			<p style="font-weight: bold">
+				<ul>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/archives/Q32014/incubation">http://download.eclipse.org/technology/nebula/archives/Q32014/incubation</a></li>
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/archives/Q32014/incubation/repository.zip">repository.zip</a></li>
+				</ul>
+			</p>
+			
+						<br/>
+			<br/>
+			<h3>Q1/2014. Retention period: 2 years.</h3> 
+			<h4>Nebula Release (12 Projects)</h4>
+			<p style="font-weight: bold">
+				<ul>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/archives/Q12014/release/">http://download.eclipse.org/technology/nebula/archives/Q12014/release/</a></li> 
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/archives/Q12014/release/repository.zip">repository.zip</a></li>
+				</ul>
+			</p>
+
+			<h4>Nebula Incubation (9 Projects)</h4>
+			<p style="font-weight: bold">
+				<ul>
+					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/archives/Q12014/incubation">http://download.eclipse.org/technology/nebula/archives/Q12014/incubation</a></li>
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/technology/nebula/archives/Q12014/incubation/repository.zip">repository.zip</a></li>
+				</ul>
+			</p>
+
+		<hr class="clearer" />
 	</div>
 
 	<div id="rightcolumn">

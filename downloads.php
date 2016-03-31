fe52b06d55224cc1c64721606bd0c777276e0aa5
Wim Jongman <wim.jongman@remainsoftware.com>
2016-03-31 22:27:26 +0200

    removed http references

--- downloads.php
+++ downloads.php
@@ -28,7 +28,7 @@
 		<h2>Latest</h2>
 
 		<h4>Nebula Release (13 Projects) <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229894' title='Drag and drop into a running Eclipse to install these Widgets'> 
-		<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></a></h4>
+		<img src='https://marketplace.eclipse.org/misc/installbutton.png'/></a></h4>
 			<p style="font-weight: bold">
 				<ul>
 					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/snapshot">http://download.eclipse.org/technology/nebula/snapshot</a></li>
@@ -38,7 +38,7 @@
 			</p>
 
 		<h4>Nebula Incubation (9 Projects) <a href='http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=229909' title='Drag and drop into a running to install these Widgets'> 
-		<img src='http://marketplace.eclipse.org/misc/installbutton.png'/></a></h4>
+		<img src='https://marketplace.eclipse.org/misc/installbutton.png'/></a></h4>
 			<p style="font-weight: bold">
 				<ul>
 					<li>Update site: <a href="http://download.eclipse.org/technology/nebula/incubation/snapshot">http://download.eclipse.org/technology/nebula/incubation/snapshot</a></li>
@@ -148,7 +148,7 @@
 			<div align="center">
 				<a href="http://www.eclipse.org/projects/what-is-incubation.php">
 					<img align="center"
-					src="http://www.eclipse.org/images/egg-incubation.png" border="0"
+					src="https://www.eclipse.org/images/egg-incubation.png" border="0"
 					alt="Incubation" />
 				</a>
 			</div>

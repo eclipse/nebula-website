f05f948d92a39e17b4f249d747910c0a630e54ec
Wim Jongman <wim.jongman@remainsoftware.com>
2016-05-31 23:47:02 +0200

    Bug 373009 - Nebula Website Change Requests
    Add release page

---
+++ releases/index.php
@@ -0,0 +1,38 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	# Author: 		Wim Jongman
+	# Description:  Nebula Releases page.
+
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Nebula Release 1.0.0";
+	$pageKeywords	= "Nebula, SWT, Widgets, Controls, Release";
+	$pageAuthor		= "Nebula Committers";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		<br/>
+		
+		<h2>
+			<p style="background-color:lightgrey;>
+				<a href="1.0.0/index.php">Release 1.0.0 (Release date 1st June 2016</a>
+			</p>
+		</h2>
+	
+EOHTML;
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>

15ae95a155ca7621ce7689a3f511c807870bdeb2
Wim Jongman <wim.jongman@remainsoftware.com>
2017-03-21 16:14:29 +0100

    Bug 373009 - Nebula Website Change Requests
    Added visualization image

--- index.php
+++ index.php
@@ -363,7 +363,7 @@
 							<tr>
 								<td align="center"><a
 									href="widgets/visualization/visualization.php">
-										<img src="widgets/visualization/images/Combined.png" width="600 height="600" border="0"
+										<img src="images/visualization.png" border="0"
 										alt="Visualization XY Graph."
 										title="Visualization">
 								</a></td>

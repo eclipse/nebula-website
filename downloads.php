def35958dc04a69cca5b298cdc2f898d50076cad
Wim Jongman <wim.jongman@remainsoftware.com>
2018-03-23 00:18:10 +0100

    Bug 531386 Publish 2.0.0 javadoc
    * Published javadoc

--- downloads.php
+++ downloads.php
@@ -38,7 +38,8 @@
 		
 		<h2>Releases</h2>
 			<ul>
-			    <li><a href='releases/1.4.0/index.php'>Release 1.4.0</a></li>
+			    <li><a href='releases/2.0.0/index.php'>Release 2.0.0</a></li>
+			    <li><b><a href='releases/1.4.0/index.php'>Release 1.4.0</a></b></li>
 			    <li><a href='releases/1.3.0/index.php'>Release 1.3.0</a></li>
 				<li><a href='releases/1.2.0/index.php'>Release 1.2.0</a></li>
 				<li><a href='releases/1.1.0/index.php'>Release 1.1.0</a></li>
---
+++ releases/2.0.0/index.php
@@ -0,0 +1,45 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	# Author: 		Wim Jongman
+	# Description:  Nebula Releases page.
+
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Nebula Release 1.4.0";
+	$pageKeywords	= "Nebula, SWT, Widgets, Controls, Release";
+	$pageAuthor		= "Nebula Committers";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		<br/>
+		<h1 style="color: red;">RELEASE NOT FINAL UNTIL MARCH 31 2018</h1>
+		<p>
+		This is the Nebula <b>2.0.0</b> release. <a href="https://bugs.eclipse.org/bugs/buglist.cgi?order=component%2Cpriority%2Cbug_severity&product=Nebula&query_based_on=&query_format=advanced&target_milestone=2.0.0" target=blank>We resolved 26 issues in this release and added 26 new widgets.</a>  
+		</p>
+
+		<h3>Downloads</a></h3>
+			<p style="font-weight: bold">
+				<ul>
+					<li>Update site: <a href="http://download.eclipse.org/nebula/releases/2.0.0">http://download.eclipse.org/nebula/releases/2.0.0</a></li>
+					<li>Update site repo zipped: <a href="http://download.eclipse.org/nebula/releases/2.0.0/repository-nebula-2.0.0.zip">repository.zip</a></li>
+					<li>Javadoc:  <a href="javadoc/index.html">Link to Javadoc</a></li>
+				</ul>
+			</p>	
+	
+EOHTML;
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
